fa419b85c22e5c1b5cc877875ab1aa22372c3cc3
Parmjeet_Singh <gurufateh22@gmail.com>
2021-05-15 15:03:46 +0530

    check list length

--- src/users.js
+++ src/users.js
@@ -4,25 +4,18 @@ import React, { useState, useEffect } from "react";
 function Users()
 {
     // const [name, setName] = useState("navroop");
-  const user=[
-    { name: "parmjeet", age: 25, address: "noida" },
-    { name: "jeet", age: 25 },
-    { name: "sing", age: 20 },
-    { name: "roop", age: 15 },
-    { name: "nav", age: 10 },
-  ]
+  const [user,setUser]=useState([
+    { name: 'parmjeet', age: '25', address: 'noida' },
+    { name: 'jeet', age: '25' },
+    { name: 'sing', age: '20' },
+    { name: 'roop', age: '15' },
+    { name: 'nav', age: '10' }
+  ])    
   return(
       <div>
           <h1>hello listing is here</h1>
     {
-    user.map((item, i) => 
-        // <div key={i}>  {item.name} , {item.age} ,{item.address} </div>
-        <div key={i}>
-          <span>{item.name}</span>
-          <span>{item.age}</span>
-          <span>{item.address}</span>
-        </div>
-      ) 
+        user.length==55?<div>yes ,  this is right</div>:<div>No length should be 5 </div>
     }
         </div>
     )
