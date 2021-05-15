1fdb3f3995e298b7842e362ad840a42748e38060
Parmjeet_Singh <gurufateh22@gmail.com>
2021-05-15 19:02:42 +0530

    react routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,61 @@
-
 import About from "./About";
 import Contact from "./Contact";
 import Users from "./users";
 import "./App.css";
 import CreateUser from "./CreateUser";
+import { Link, Route, BrowserRouter as Router,Switch} from "react-router-dom";
+
+import { Navbar, Nav } from "react-bootstrap";
+import Home from "./Home";
 
 function App() {
-  
   return (
     <div className="App">
-      <h1>hello from react</h1>
-      {/* <About name={name} /> */}
+      <Router>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">
+              {" "}
+              <Link to="/">Home</Link>
+            </Nav.Link>
+            <Nav.Link href="#features">
+              {" "}
+              <Link to="/about">About</Link>
+            </Nav.Link>
+            <Nav.Link href="#pricing">
+              <Link to="/contact">Contact</Link>
+            </Nav.Link>
+            <Nav.Link href="#pricing">
+              <Link to="/list">User List</Link>
+            </Nav.Link>
+            <Nav.Link href="#pricing">
+              <Link to="/create">Create user</Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/list">
+            <Users />
+          </Route>
+          
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          
+          <Route path="/create">
+            <CreateUser />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
 
-      {/* <Contact /> */}
-      {/* <Users /> */}
-      <CreateUser />
+       
+      </Router>
     </div>
   );
 }
