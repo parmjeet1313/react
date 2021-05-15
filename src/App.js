83f976d27105fe11916cbdaa1baff0134e5972ec
Parmjeet_Singh <gurufateh22@gmail.com>
2021-05-15 11:56:26 +0530

    props in react

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 import About from "./About";
 import Contact from "./Contact";
 import "./App.css";
 
 function App() {
+  const[name,setName]=useState("navroop")
   return (
     <div className="App">
       <h1>hello from react</h1>
+      {/* <About name={name} /> */}
+      <button onClick={()=>{setName("Rupinder")}}>update props</button>
       {/* <About /> */}
-      {/* <About /> */}
-      <Contact />
+      <Contact name={name}/>
+
     </div>
   );
 }
--- src/Contact.js
+++ src/Contact.js
@@ -1,21 +1,23 @@
 import React,{useEffect,useState} from "react";
-function Contact() {
+function Contact(props) {
 
-  const[name,setName]=useState("raman")
-  const[age,setAge]=useState(25)
+  // const[name,setName]=useState("raman")
+  // const[age,setAge]=useState(25)
   useEffect(()=>{
-    console.warn("hello from Hook")
-  },[age])
+    console.warn("check props",props.name)
+  },[])
+  useEffect(()=>{
+    console.warn("check props 2 for update",props.name)
+  },[props.name])
+
 
   let data = "this is jsx contact component";
   return (
     <div>
       <h1>{data}</h1>
-      <h1>{name}</h1>
-      <h1>{age}</h1>
-      <button onClick={()=>setName("ramandeep singh")}>Update Name</button>
-      <button onClick={()=>setAge(52)}>update Age</button>
+      <h2>{props.name}</h2>
 
+      
     </div>
   );
 }
